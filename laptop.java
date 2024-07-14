be75307e6c6876cda32d462a8c684879403798f0
Kira <kirnan_t@outlok.com>
2024-07-14 13:18:04 +0700

    public laptop

--- laptop.java
+++ laptop.java
@@ -1,3 +1,17 @@
+import java.util.*;
+
 public class laptop {
-    
+   private String модель;
+   private int объемОЗУ;
+   private int объемЖД;
+   private String  операционнаяСистема;
+   private String цвет;
+   
+   public laptop (String модель, int объемОЗУ, int объемЖД, String  операционнаяСистема, String цвет;){
+        this.модель = модель;
+        this.объемОЗУ = объемОЗУ;
+        this.объемЖД = объемЖД;
+        this.операционнаяСистем = операционнаяСистем;
+        this.цвет = цвет;
+    }
 }
